3b3cb1205167313f70d4205f0142dbb84ecb586e
Roman Maruk <maruchok@gmail.com>
2024-07-11 13:20:07 +0300

    initial code

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+const PORT = 3001;
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello</h1>');
+});
+app.listen(PORT, () => {
+  console.log(`Port lisent on port ${PORT}`);
+});
